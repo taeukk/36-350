6409e838b5b638dbb6a6d4b057d2ee6f392d3c71
taeukk <42041621+taeukk@users.noreply.github.com>
2018-08-03 12:26:09 -0400

    Update lab_12_simulation.R

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,3 +1,16 @@
 generate_data <- function(n, p) {
 return(list(covariates = matrix(rnorm(n * p), n, p), responses = rnorm(n)))
 }
+
+model_select <- function(covariates, responses, cutoff) {
+  model <- lm(responses ~ covariates)                             
+  retained <- summary(model)$coefficients[-1,4] <= cutoff         
+  
+  if (!any(retained)) {
+return(c())                                                 
+  }
+  
+  model <- lm(responses ~ covariates[, retained])                   
+  return(as.vector(summary(model)$coefficients[-1, 4]))
+
+}
