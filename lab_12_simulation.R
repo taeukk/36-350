82103df52f21c444f7723563ff1e2fe3d3c12952
taeukk <42041621+taeukk@users.noreply.github.com>
2018-08-03 16:07:44 -0400

    Update lab_12_simulation.R

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -14,3 +14,16 @@ return(c())
   return(as.vector(summary(model)$coefficients[-1, 4]))
 
 }
+
+run_simulation <- function(n_trials, n, p, cutoff) {                       
+  replicates <- replicate(n_trials, {                              
+    data <- generate_data(n, p)                                           
+    return(model_select(data$covariates, data$responses, cutoff))         
+    })                                                                     
+  return(c, replicates)                                  
+}                                                            
+                                                                         
+combination <- expand.grid(n = c(100, 1000, 10000),                            
+                      p = c(10, 20, 50),                                  
+                      n_trials = 100,                                   
+                      cutoff = 0.05)    
